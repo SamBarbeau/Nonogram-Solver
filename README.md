b9db57148a3918a351f0f4a4155e7e2bd987db11
SamBarbeau <samueljosephbarbeau@gmail.com>
2024-03-12 21:51:36 -0500

    add cache dictionary for puzzles. add new way to run the program - see updated README

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Overview
 
-I have played the Nonograms app on my iphone for quite some time. After my final exams one semester, I found there was an online version (which probably predated the iphone app), at https://www.puzzle-nonograms.com/, and decided to find a way to automate the puzzles.
+I have played the Nonograms app on my iphone for quite some time. After my final exams one semester, I found there was an online version (which probably predated the iphone app), at https://www.puzzle-nonograms.com/, and decided to find a way to automate the puzzles. If you are concerned that I am cheating, don't worry, there is a robot mode that is activated when a hidden input's value is set to true. When this is active, all times are sent to the 'ROBOTS hall of fame.'
 
 ## Features
 
@@ -27,7 +27,7 @@ Hennie explains there are three general steps in the process.
 - Fill the cells that you know must be filled (the intersection of all the options for that row / column)
 - Remove options based on step 2
 
-In step 2, I also "unfilled" (put an X) cells I knew must be unfilled (the cell doesn't appear in the union of all the options for that row / column).
+In step 2, I also 'unfilled' (put an X) cells I knew must be unfilled (the cell doesn't appear in the union of all the options for that row / column).
 I solved the puzzle using a NumPy array. After solving the puzzle, I use Selenium to access the cell elements on the website and click the cells that should be filled. Visually, the puzzle will always appear to be solved top to bottom. An optimization that could be implemented is to fill the cells as the puzzle is being solved (though I don't think it would do much).
   
 Currently, I don't plan on adding more to this program... but who knows.
@@ -36,8 +36,8 @@ Currently, I don't plan on adding more to this program... but who knows.
 
 To use the program, go to the nonograms website linked above.
 - Find a puzzle you want to solve
-- Press the "Share" button under the puzzle
-- Copy the "Progress Permalink", you'll need this permalink when prompted by the command line for setup questions.
+- Press the 'Share' button under the puzzle
+- Copy the 'Progress Permalink', you'll need this permalink when prompted by the command line for setup questions.
 Now, run
 ```
 python3 nonogram_solver.py
@@ -48,4 +48,12 @@ The program prompts the user for the following information:
 - Whether to solve the given puzzle or find a new one (resetting the timer)
 - Number of puzzles to solve in a row
 
-If the puzzle is a special puzzle, the last two questions are defaulted to "no" and "1" respectively since speical puzzles don't have a "New Puzzle" button.
+If the puzzle is a special puzzle, the last two questions are defaulted to 'no' and '1' respectively since speical puzzles don't have a 'New Puzzle' button.
+
+*Alternatively,* if you have the 'Nonograms Puzzle ID' and the size, you can check the cache to see if you've solved the puzzle before by running
+```
+python3 nonogram_solver.py <id> <size_key>
+```
+The 'id' can be with commas or without. The 'size_key' is defined as follows: 0 for 5x5, 1 for 10x10, 2 for 15x15, 3 for 20x20, 4 for 25x25. This is more for fun; running the program like this will forfeit your opportunity to submit the time to the (robots) hall of fame. The program automatically checks all puzzles if they've been cached (solved before), even if the program is not ran like this. Of course, it is unlikely to randomly get a puzzle you've seen before. 
+
+Note: the special puzzles do not have a puzzle id, so the program only caches the regular puzzles.
--- nonogram_solver.py
+++ nonogram_solver.py
@@ -5,13 +5,15 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service
 import numpy as np
+import os
+import sys
 import time
 from utils import *
 
 
 def solve_nonogram(driver, solve_starting_puzzle):
     # press the "New Puzzle" button to reset timer (and find new puzzle)
-    if solve_starting_puzzle:
+    if not solve_starting_puzzle:
         new = driver.find_element(By.ID, "btnNew")
         new.click()
 
@@ -51,28 +53,70 @@ def solve_nonogram(driver, solve_starting_puzzle):
         # step 3: remove options based on filled or unfilled (1 or -1) cells
         remove_options(nonogram_grid, row_options, col_options)
 
-    return nonogram_grid
+    return nonogram_grid, n_rows # need n_rows for cache dictionary
 
 
-
-if __name__ == "__main__":
+# Collect User Input
+def user_input():
     # url of puzzle to solve (take the "Progress Permalink:" in the "Share" drop down)
     # this will not be the puzzle solved if the "New Puzzle" button is pressed (see first few lines of solve_nonogram function)
     puzzle_link = input("Input the peramlink of the puzzle: ")
 
     # If the puzzle is a speical Daily, Weekly, or Monthly puzzle, there is no "New Puzzle" button
     is_special_puzzle = input("\n***\n\nIs this a special Daily, Weekly, or Monthly puzzle? (y/n): ").lower()
+    is_special_puzzle = True if is_special_puzzle == 'y' else False
 
-    if is_special_puzzle == 'n':
+    if not is_special_puzzle:
         # Since the time is already running on the puzzle, the user can choose to reset the time by finding a new puzzle
         solve_starting_puzzle = input("\n***\n\nThe time is already running on the puzzle.\nWant to start by finding a new puzzle so time resets? (y/n): ")
-        solve_starting_puzzle = True if solve_starting_puzzle.lower() == 'y' else False
+        solve_starting_puzzle = False if solve_starting_puzzle.lower() == 'y' else True
 
         # number of mazes to solve (iterations in the loop below)
         num_mazes_solve = int(input("\n***\n\nHow many puzzles do you want to solve in a row: "))
     else:
-        solve_starting_puzzle = False
+        solve_starting_puzzle = True
         num_mazes_solve = 1
+    
+    return puzzle_link, is_special_puzzle, solve_starting_puzzle, num_mazes_solve
+
+# The special puzzle page takes a puzzle_id and the size of the puzzle
+def specific_puzzle(driver, puzzle_id, size_key):
+    size_select = driver.find_element(By.ID, "size")
+    options = size_select.find_elements(By.TAG_NAME, "option")
+    for option in options:
+        if option.get_attribute("value") == str(size_key):
+            option.click()
+            break
+
+    puzzle_input = driver.find_element(By.NAME, "specid")
+    driver.execute_script(f"arguments[0].value = '{puzzle_id}';", puzzle_input)
+
+    # Then we click the "Choose" button
+    button = driver.find_element(By.CSS_SELECTOR, 'input[type="submit"][value="   Choose   "]')
+    button.click()
+
+
+if __name__ == "__main__":
+    # check if command line arguments were passed
+    if len(sys.argv) > 1:
+        # if so, the first argument is the puzzle ID
+        puzzle_id = sys.argv[1]
+        size_key = int(sys.argv[2]) # 5x5 = 0, 10x10 = 1,..., 25x25 = 4
+        puzzle_link = 'https://www.puzzle-nonograms.com/specific.php'
+        num_mazes_solve = 1
+        solve_starting_puzzle = True
+        is_special_puzzle = False
+    else:
+        # Collect User Input
+        puzzle_link, is_special_puzzle, solve_starting_puzzle, num_mazes_solve = user_input()
+    
+    # Check if the cache file exists
+    if os.path.exists('solved_puzzles_cache.npy'):
+        # Load the cache from the file
+        solved_puzzles_cache = np.load('solved_puzzles_cache.npy', allow_pickle=True).item()
+    else:
+        # Initialize the cache dictionary
+        solved_puzzles_cache = {}
 
     # chromedriver executable
     chromedriver_path = '/Users/sambarbeau/Documents/code/python/nonogram solver/driver/chromedriver'
@@ -91,16 +135,39 @@ if __name__ == "__main__":
     driver.get(puzzle_link)
 
     for i in range(num_mazes_solve):
-        nonogram_grid = solve_nonogram(driver,solve_starting_puzzle)
-        update_website_grid(driver, nonogram_grid)
+        if len(sys.argv) == 1:
+            if not is_special_puzzle:
+                # get the puzzle size and id for the cache
+                puzzle_id = driver.find_element(By.ID, "puzzleID").text
+
+                size_info = driver.find_element(By.CSS_SELECTOR, 'div.puzzleInfo p').text[:2]
+                size = int(size_info[0]) if size_info.startswith('5') else int(size_info) # could be '5x' or '10','15','20','25
+                size_key = int(size/5) - 1      # 5x5 = 0, 10x10 = 1,..., 25x25 = 4
+            else:
+                puzzle_id = -1
+                size_key = -1
+        else:
+            # Here, we are in the choose specific puzzle page, we need to input the puzzle size and puzzle id
+            specific_puzzle(driver, puzzle_id, size_key)
+
+        # check if the puzzle has already been solved (and cached)
+        if (puzzle_id,size_key) in solved_puzzles_cache:
+            update_website_grid(driver, solved_puzzles_cache[(puzzle_id,size_key)])
+        else:
+            nonogram_grid, size = solve_nonogram(driver,solve_starting_puzzle)
+            update_website_grid(driver, nonogram_grid)
+            
+            if not is_special_puzzle:
+                # Save the solved puzzle to the cache
+                solved_puzzles_cache[(puzzle_id,size_key)] = nonogram_grid
 
         # uncomment if you want to see the puzzle finished before going to next puzzle
         time.sleep(3)
 
         # click the "New Puzzle" after solving the puzzle
-        if not solve_starting_puzzle and i < num_mazes_solve - 1:
+        if solve_starting_puzzle and i < num_mazes_solve - 1:
             new = driver.find_element(By.ID, "btnNew")
             new.click()
-
-    # this is to keep the browser from closing after completing (doens't always close -- may depend on device?)
-    # input("-- press enter to close --")
+    
+    # Save the cache to a file
+    np.save('solved_puzzles_cache.npy', solved_puzzles_cache)
